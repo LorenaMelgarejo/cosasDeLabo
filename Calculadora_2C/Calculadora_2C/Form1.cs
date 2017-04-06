06d77bc8f172bca635e11725daa5bf1aa9c31092
LorenaMelgarejo <lorena_melgarejo@yahoo.com.ar>
2017-04-07 01:05:43 +0200

    Add files via upload

---
+++ Calculadora_2C/Calculadora_2C/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_2C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOperacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            cmbOperacion.Text = "";
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+
+            Numero Nv1 = new Numero(this.txtNumero1.Text);
+            Numero Nv2 = new Numero(this.txtNumero2.Text);
+
+            this.lblResultado.Text = Calculadora.operar(Nv1, Nv2, cmbOperacion.Text).ToString();
+
+            this.lblResultado.Visible = true;
+        }
+    }
+}
---
+++ Calculadora_2C/Calculadora_2C/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_2C
+{
+     public static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Numero num = new Numero();
+            Calculadora cal = new Calculadora();
+
+        }
+
+    }
+}
